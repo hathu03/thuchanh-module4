f77551502e84c40299aa66eef0ad19ae9bfcfcf8
hathu03 <nguyenthuha2411vmh@gmail.com>
2022-01-26 13:39:47 +0700

    test-module4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { AwesomeListComponent } from './components/awesomes/awesome-list/awesome-list.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { AwesomeCreateComponent } from './components/awesomes/awesome-create/awesome-create.component';
+import { AwesomeUpdateComponent } from './components/awesomes/awesome-update/awesome-update.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// // @ts-ignore
+// import {MatButtonModule} from "@angular/material/button";
+// // @ts-ignore
+// import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AwesomeListComponent,
+    AwesomeCreateComponent,
+    AwesomeUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    // MatButtonModule,
+    // MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// @ts-ignore
 export const environment = {
-  production: false
+  production: false,
+  api_url: "http://localhost:3000"
 };
 
 /*
